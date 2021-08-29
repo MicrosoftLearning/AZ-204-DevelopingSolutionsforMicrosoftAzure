693816dde99054b7f0381ff703f56cefbf27e4d4
Your Name <polichtm@hotmail.com>
2021-08-29 11:18:20 -0400

    l04-labfiles

--- Allfiles/Labs/04/Starter/AdventureWorks.Web/Startup.cs
+++ Allfiles/Labs/04/Starter/AdventureWorks.Web/Startup.cs
@@ -30,11 +30,12 @@ namespace AdventureWorks.Web
 
         public void ConfigureProductService(IServiceCollection services)
         {            
-            services.AddScoped<IAdventureWorksProductContext, AdventureWorksSqlContext>(provider =>
-                new AdventureWorksSqlContext(
-                    _configuration.GetConnectionString(nameof(AdventureWorksSqlContext))
-                )
-            );
+            services.AddScoped<IAdventureWorksProductContext, AdventureWorksCosmosContext>(provider =>
+    new AdventureWorksCosmosContext(
+        _configuration.GetConnectionString(nameof(AdventureWorksCosmosContext))
+    )
+);
+
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
