bdf079ac4dc8c2a41b265f64d6ddb350c44b710e
v-asamim <v-asamim@microsoft.com>
2023-07-27 14:40:13 +0100

    Lab 13 instructions and web project updated

--- Allfiles/Labs/13/Starter/webapp-sam-lab13/Pages/Privacy.cshtml.cs
+++ Allfiles/Labs/13/Starter/webapp-sam-lab13/Pages/Privacy.cshtml.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace myWebApp.Pages;
+namespace webapp_sam_lab13.Pages;
 
 public class PrivacyModel : PageModel
 {
